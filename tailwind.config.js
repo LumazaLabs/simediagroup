549fe93ee808950a7200ac88f79b1e958aaf302c
lukebricknell <lukecbricknell@gmail.com>
2023-02-08 08:55:37 +0200

    Added access control and connected all buttons

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        yellow: "#FCB116",
+        blue: "#000829",
+      },
+    },
+  },
+  plugins: [],
+};
